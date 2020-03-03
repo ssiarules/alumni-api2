2923fc6fb668b304ec80214ff70491a4399da47f
Johnson <jhonsonj88@gmail.com>
2020-03-03 16:22:32 -0500

    crated sessions, registrations, concern/current_user and application controller, created logged_in and logout methods

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,7 @@
 class SessionsController < ApplicationController
+   #make sure that current user is in session
+    include CurrentUserConcern
+
 
     def create 
     user = User
@@ -8,7 +11,7 @@ class SessionsController < ApplicationController
 #if the user was created communicate with the session, we want to set the user id = to the user ID we just found, this will implement the cookie in the user browser.
 #if user not able to log in it will get a status of 401 meaning they are not aunthenticated. 
             if user 
-                    session[:user_id] = user.user_id
+                    session[:user_id] = user.id
                     render json: {
                         status: :created, 
                         logged_in: true, 
@@ -18,4 +21,25 @@ class SessionsController < ApplicationController
                 render json: { status: 401}
          end 
     end 
+
+#if user is logged in, we see their session ID,and we send it back with their user account
+#if they are logged in or current user  
+        def logged_in 
+            if @current_user
+            render json: {
+                logged_in: true, 
+                user: @current_user
+            }
+            else  
+            render json: {
+                logged_in: false 
+            }
+            end 
+        end 
+
+        def logout 
+            reset_session
+            render json: { status: 200, logged_out: true }
+
+        end 
 end 
